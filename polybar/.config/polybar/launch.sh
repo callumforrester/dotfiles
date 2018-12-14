40ae99d276482bbc7ea45e65b6921ae0e14c3d9c
Callum Forrester <caf32@kent.ac.uk>
2018-12-14 23:58:07 +0000

    Made polybar multihead compatible

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
-# Launch bar1
+# Figure out primary monitor
+PRIMARY=$(xrandr -q | grep " connected primary" | cut -d" " -f1)
 
-polybar bar1 &
+# Launch bar1
+echo "Primary monitor is $PRIMARY"
+MONITOR=$PRIMARY polybar bar1 &
 
 echo "Bars launched..."
